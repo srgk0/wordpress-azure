3443e032f7a4b9a3d35000bf1e337d86f4f15342
srgk0 <ser414.93@mail.ru>
2016-11-14 11:01:50 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,85 @@
-<?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
-
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define('WP_USE_THEMES', true);
-
-/** Loads the WordPress Environment and Template */
-require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+<html>
+<head>
+<Title>Registration Form</Title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<style type="text/css">
+    body { background-color:
+ #fff; border-top: solid 10px #000;
+ color: #333; font-size: .85em;
+ margin: 20; padding: 20;
+ font-family: "Segoe UI",
+ Verdana, Helvetica, Sans-Serif;}
+    h1, h2, h3,{ color: #000; 
+margin-bottom: 0; padding-bottom: 0; }
+    h1 { font-size: 2em; }
+    h2 { font-size: 1.75em; }
+    h3 { font-size: 1.2em; }
+    table { margin-top: 0.75em; }
+    th { font-size: 1.2em;
+ text-align: left; border: none; padding-left: 0; }
+    td { padding: 0.25em 2em 0.25em 0em; 
+border: 0 none; }
+</style>
+</head>
+  <body>
+<form>    
+<h1>Регистрация</h1>
+<p>Заполните ваше имя и адрес электронной почты, затем нажмите кнопку Отправить, чтобы зарегистрироваться.</p>
+<form method="post" action="index.php" 
+enctype="multipart/form-data" >
+       Имя  <input type="text" 
+name="name" id="name"/></br>
+      Email <input type="text" 
+name="email" id="email"/></br>
+      <input type="submit" 
+name="submit" value="Отправить" />
+</form>
+try {
+    $conn = new PDO
+( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
+    $conn->setAttribute
+( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+}
+catch(Exception $e){
+    die(var_dump($e));
+}
+if(!empty($_POST)) {
+try {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $date = date("Y-m-d");
+    // Insert data
+    $sql_insert = 
+"INSERT INTO registration_tbl (name, email, date) 
+                   VALUES (?,?,?)";
+    $stmt = $conn->prepare($sql_insert);
+    $stmt->bindValue(1, $name);
+    $stmt->bindValue(2, $email);
+    $stmt->bindValue(3, $date);
+    $stmt->execute();
+}
+catch(Exception $e) {
+    die(var_dump($e));
+}
+echo "<h3> Вы зарегистрированы! </h3>";
+}
+$sql_select = "SELECT * FROM registration_tbl";
+$stmt = $conn->query($sql_select);
+$registrants = $stmt->fetchAll(); 
+if(count($registrants) > 0) {
+    echo "<h2>People who are registered:</h2>";
+    echo "<table>";
+    echo "<tr><th>Name</th>";
+    echo "<th>Email</th>";
+    echo "<th>Date</th></tr>";
+    foreach($registrants as $registrant) {
+        echo "<tr><td>".$registrant['name']."</td>";
+        echo "<td>".$registrant['email']."</td>";
+        echo "<td>".$registrant['date']."</td></tr>";
+    }
+    echo "</table>";
+} else {
+    echo "<h3>No one is currently registered.</h3>";
+}
+</body>
+</html>
